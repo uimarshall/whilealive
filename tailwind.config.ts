9e8e530ef066a63e1101ae6746db0a3a5dbe307a
uimarshall <uimarshall@gmail.com>
2023-09-10 23:42:30 +0100

    Add global css class configurations.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,171 @@ const config: Config = {
   // },
 
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    fontSize: {
+      'heading1-bold': [
+        '36px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'heading1-semibold': [
+        '36px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'heading2-bold': [
+        '30px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'heading2-semibold': [
+        '30px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'heading3-bold': [
+        '24px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'heading4-medium': [
+        '20px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'body-bold': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'body-semibold': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'body-medium': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'body-normal': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '400',
+        },
+      ],
+      'body1-bold': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'base-regular': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '400',
+        },
+      ],
+      'base-medium': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'base-semibold': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'base1-semibold': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'small-regular': [
+        '14px',
+        {
+          lineHeight: '140%',
+          fontWeight: '400',
+        },
+      ],
+      'small-medium': [
+        '14px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'small-semibold': [
+        '14px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'subtle-medium': [
+        '12px',
+        {
+          lineHeight: '16px',
+          fontWeight: '500',
+        },
+      ],
+      'subtle-semibold': [
+        '12px',
+        {
+          lineHeight: '16px',
+          fontWeight: '600',
+        },
+      ],
+      'tiny-medium': [
+        '10px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'x-small-semibold': [
+        '7px',
+        {
+          lineHeight: '9.318px',
+          fontWeight: '600',
+        },
+      ],
+    },
     extend: {
+      // This is where we extend the base css properties or classes, e.g. we can define additional colors apart from the default ones from tailwind.css.
       colors: {
         'primary-5': '#FFF5D8',
         'primary-10': '#FF6600',
@@ -64,13 +228,33 @@ const config: Config = {
         patternbackground: 'url(./assets/background_pattern_bottom.png)',
       },
     },
+
+    boxShadow: {
+      'count-badge': '0px 0px 6px 2px rgba(219, 188, 159, 0.30)',
+      'groups-sidebar': '-30px 0px 60px 0px rgba(28, 28, 31, 0.50)',
+    },
+
+    // keyframes: {
+    //   'accordion-down': {
+    //     from: { height: 0 },
+    //     to: { height: 'var(--radix-accordion-content-height)' },
+    //   },
+    //   'accordion-up': {
+    //     from: { height: 'var(--radix-accordion-content-height)' },
+    //     to: { height: 0 },
+    //   },
+    // },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
+    },
     screens: {
-      xs: '480px',
+      xs: '400px',
       sm: '768px',
       md: '1060px',
     },
   },
 
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
