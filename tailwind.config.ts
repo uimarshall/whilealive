8d38a3c0331d113e42eddf6cfa23a0222f3c4652
uimarshall <uimarshall@gmail.com>
2023-09-12 21:53:35 +0100

    Add file system routing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -204,6 +204,20 @@ const config: Config = {
         'gray-300': '#423B39',
         'gray-400': '#727274',
         'gray-500': '#423B39',
+
+        blue: '#0095F6',
+        'logout-btn': '#FF5A5A',
+        'navbar-menu': 'rgba(16, 16, 18, 0.6)',
+        'dark-1': '#000000',
+        'dark-2': '#121417',
+        'dark-3': '#101012',
+        'dark-4': '#1F1F22',
+        'light-1': '#FFFFFF',
+        'light-2': '#EFEFEF',
+        'light-3': '#7878A3',
+        'light-4': '#5C5C7B',
+        'gray-1': '#697C89',
+        glassmorphism: 'rgba(16, 16, 18, 0.60)',
       },
       backgroundImage: (theme) => ({
         'gradient-lightgray-1':
