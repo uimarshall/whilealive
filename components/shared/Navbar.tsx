800e73d469ba9acd0a144981f12c65d126b2074b
uimarshall <uimarshall@gmail.com>
2023-09-20 23:40:26 +0100

    Add Navbar components with the logo.

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,11 +1,34 @@
+import { SignedIn } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 function Navbar() {
+  const isUserLoggedIn = true;
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <nav className="navbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image
+          src="/assets/whileAliveLogo.png"
+          alt="WhileAlive Logo"
+          width={28}
+          height={28}
+        />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          WhileAlive
+        </p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn></SignedIn>
+          {}
+        </div>
+        <Link href="/login"></Link>
+        <Link href="/signup"></Link>
+      </div>
+    </nav>
   );
 }
+Navbar;
 
 export default Navbar;
