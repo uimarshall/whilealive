a5e0cb6cf44b8a85810de0c5d8228f868bd298d1
uimarshall <uimarshall@gmail.com>
2023-09-28 22:41:06 +0100

    Add constants for the app

---
+++ constants/index.js
@@ -0,0 +1,18 @@
+export const sidebarLinks = [
+  {
+    name: 'Home',
+    path: '/',
+    icon: 'home',
+    imageURL: 'http://www.lorempixel.com/400/200/',
+  },
+  {
+    name: 'About',
+    path: '/about',
+    icon: 'info',
+  },
+  {
+    name: 'Contact',
+    path: '/contact',
+    icon: 'contact_mail',
+  },
+];
